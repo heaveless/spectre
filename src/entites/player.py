1c75773802542b5930b1210339665ecfc396e5f9
Alberto Llamocca <user_tekton06@skyairline.com>
2021-06-27 20:00:47 -0500

    fixed

--- src/entites/player.py
+++ src/entites/player.py
@@ -1,9 +1,15 @@
+import pygame as pg
+
 class Player():
-  def __init__(self):
-    pass
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
 
   def update(self):
     pass
 
   def draw(self, surface):
-    pass
+    rect = pg.Surface([100, 100])
+    rect.fill((255, 255, 255))
+
+    surface.blit(rect, (self.x, self.y))
--- src/scenes/level.py
+++ src/scenes/level.py
@@ -1,15 +1,24 @@
 import pygame as pg
 
+from ..entites.player import Player
+
 class Level():
   def __init__(self, resources):
     self.json_maps = resources.load_all_json_maps()
     self.image_maps = resources.load_all_image_maps()
     self.__load_level()
+    self.__load_hero()
 
   def __load_level(self, level = 0):
     index = str(level)
     self.current_level = self.json_maps[index]
     self.current_background = self.image_maps[index]
+
+  def __load_hero(self):
+    start = self.current_level["start"]
+    x = start["x"]
+    y = start["y"]
+    self.player = Player(x, y)
 
   def __restart_level(self):
     pass
@@ -21,7 +30,11 @@ class Level():
     surface.blit(self.current_background, (0, 0))
 
     for layer in self.current_level["layers"]:
-      rect = pg.Surface([layer["width"], layer["height"]])
-      rect.fill((255, 255, 255))
+      width = layer["width"]
+      height = layer["height"]
+      x = layer["x"]
+      y = layer["y"]
 
-      surface.blit(rect, (layer["x"], layer["y"]))
+      rect = pg.Surface([width, height])
+      rect.fill((255, 255, 255))
+      surface.blit(rect, (x, y))
