5df93bc6cd3caa0f54339bf1421ea915adb06fe4
Camila <camilaclary@gmail.com>
2021-06-27 21:37:07 -0500

    no he hecho nada

--- src/common/enums/direction_type.py
+++ src/common/enums/direction_type.py
@@ -3,3 +3,4 @@ from enum import IntEnum
 class DirectionType(IntEnum):
   RIGHT = 0
   LEFT = 1
+  UP = 2
--- src/entites/player.py
+++ src/entites/player.py
@@ -6,7 +6,8 @@ class Player():
   def __init__(self, x, y):
     self.x = x
     self.y = y
-    self.velocity = 5
+    self.gravity = .35
+    self.velocity = pg.math.Vector2(4, 0)
 
   def __pressed(self):
     keys = pg.key.get_pressed()
@@ -15,23 +16,24 @@ class Player():
     if keys[pg.K_a]:
       self.__run(DirectionType.LEFT)
     if keys[pg.K_w]:
-      self.__jump()
-
-  def __gravity(self):
-    self.set_location(self.x, self.y + self.velocity)
+      self.vertical_movement(DirectionType.UP)
 
   def __run(self, type):
     if type == DirectionType.RIGHT:
-      self.set_location(self.x + self.velocity, self.y)
+      self.x += self.velocity.x  
+      self.set_location(self.x, self.y)
     elif type == DirectionType.LEFT:
-      self.set_location(self.x - self.velocity, self.y)
-
-  def __jump(self):
-    self.set_location(self.x, self.y - self.velocity)
+      self.x -= self.velocity.x
+      self.set_location(self.x, self.y)
+  
+  def vertical_movement(self, type):
+    self.velocity.y += self.gravity 
+    if type == DirectionType.UP:
+      self.velocity.y += 4
+      self.set_location(self.x, self.y + self.velocity.y)
 
   def update(self, delta_time):
     self.__pressed()
-    self.__gravity()
 
   def set_location(self, x, y):
     self.x = x
