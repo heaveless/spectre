ca710751e96a0f633990d1be3f89d2879af48a85
experimentallife <user@experimentallife.dev>
2021-06-30 07:54:10 -0500

    ✨  melancoly finally

--- src/scenes/gameover.py
+++ src/scenes/gameover.py
@@ -5,12 +5,46 @@ from ..utils.colors_util import Color
 class GameOver(SceneBase):
   def __init__(self, resources):
     SceneBase.__init__(self)
+    self.music = resources.load_all_music()
+    self.current_second = -1
     self.__initialize()
+    self.__play_music()
 
   def __initialize(self):
-    font = pg.font.Font(None, 25)
-    self.screen = font.render("You win!", True, Color.WHITE)
+    self.message = {}
+    words = [
+      "Thank you,",
+      "I'll say goodbye soon",
+      "Though its the end of the world,",
+      "Don't blame yourself now",
+      "And if its true,",
+      "I will surround you",
+      "and give life to a world",
+      "That's our own.",
+      ""
+    ]
+    self.keys = ('0', '3', '7', '10', '14', '18', '21', '26', '29')
+    for key in self.keys:
+      index = self.keys.index(key)
+      self.message[key] = words[index]
+
+
+  def __set_subtitle(self, string_index):
+    if string_index in self.keys:
+      word = self.message[string_index]
+      font = pg.font.Font(None, 25)
+      self.text = font.render(word, True, Color.WHITE)
+
+  def __play_music(self):
+    pg.mixer.music.load(self.music["thank-you"])
+    pg.mixer.music.set_volume(0.3)
+    pg.mixer.music.play()
+
+  def update(self, delta_time, time):
+    if self.current_second < time[1]:
+      self.current_second = time[1]
 
   def draw(self, surface):
-    text_rect = self.screen.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-    surface.blit(self.screen, text_rect)
+    self.__set_subtitle(str(self.current_second))
+    text_rect = self.text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    surface.blit(self.text, text_rect)
